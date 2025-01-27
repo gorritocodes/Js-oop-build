f44a98885f79fc48584413c2bbb6daae947519d7
gorritocodes <gorritocodes@gmail.com>
2025-01-27 19:54:28 -0400

    update entries.test.ts and src/ArraysMethods/index.ts

--- __tests__/ArraysMethods/entries.test.ts
+++ __tests__/ArraysMethods/entries.test.ts
@@ -1,9 +1,9 @@
 import entries from "../../src/ArraysMethods/entries";
+const array = [1, 2, 3, 4, 5];
+const iterator = entries(array);
 
 describe("The entries() method of Array instances returns a new array iterator object that contains the key/value pairs for each index in the array.", () => {
     it("should be able to return an iterator", () => {
-        const array = [1, 2, 3, 4, 5];
-        const iterator = entries(array);
         expect(iterator.next().value).toEqual([0, 1]);
         expect(iterator.next().value).toEqual([1, 2]);
         expect(iterator.next().value).toEqual([2, 3]);
