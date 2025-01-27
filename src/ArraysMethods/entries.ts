47aef14e50cab2b8f620c320d8ec42417acfced0
gorritocodes <gorritocodes@gmail.com>
2025-01-27 19:44:15 -0400

    finish Entries func.

--- src/ArraysMethods/entries.ts
+++ src/ArraysMethods/entries.ts
@@ -1,3 +1,18 @@
 export default function entries(array: any[]) {
-    return array;
+    class Iterator {
+        arr: any[];
+        index: number;
+        value: [number, any];
+        constructor(arr: any[]) {
+            this.arr = arr;
+            this.index = 0;
+            this.value = [this.index, this.arr[this.index]];
+        }
+        next() {
+            this.value = [this.index, this.arr[this.index]];
+            this.index++;
+            return this;
+        }
+    }
+    return new Iterator(array);
 }
